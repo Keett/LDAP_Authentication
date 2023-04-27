c8f73319844ce6ed94ca6d7d2b3c18ebc1d33455
Kübra Elif TOZKOPARAN <32227641+Keett@users.noreply.github.com>
2023-04-27 18:18:37 +0300

    File upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace TestAD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label11 = new System.Windows.Forms.Label();
+            this.portNumberTxt = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.slDap = new System.Windows.Forms.CheckBox();
+            this.DomainTxt = new System.Windows.Forms.TextBox();
+            this.UsernameTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PasswordTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.password_ADFS = new System.Windows.Forms.TextBox();
+            this.username_ADFS = new System.Windows.Forms.TextBox();
+            this.endPontTxt = new System.Windows.Forms.TextBox();
+            this.relyTxt = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(3, 4);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1045, 444);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label11);
+            this.tabPage1.Controls.Add(this.portNumberTxt);
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.slDap);
+            this.tabPage1.Controls.Add(this.DomainTxt);
+            this.tabPage1.Controls.Add(this.UsernameTxt);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.PasswordTxt);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Size = new System.Drawing.Size(1037, 415);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "LDAP / SLDAP";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(276, 226);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(34, 17);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Port";
+            // 
+            // portNumberTxt
+            // 
+            this.portNumberTxt.Location = new System.Drawing.Point(315, 223);
+            this.portNumberTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.portNumberTxt.Name = "portNumberTxt";
+            this.portNumberTxt.Size = new System.Drawing.Size(72, 22);
+            this.portNumberTxt.TabIndex = 11;
+            this.portNumberTxt.Text = "636";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(149, 18);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(201, 35);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Authentication";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 80);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Domain (IP)";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(144, 290);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 65);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // slDap
+            // 
+            this.slDap.AutoSize = true;
+            this.slDap.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.slDap.Location = new System.Drawing.Point(144, 215);
+            this.slDap.Margin = new System.Windows.Forms.Padding(4);
+            this.slDap.Name = "slDap";
+            this.slDap.Size = new System.Drawing.Size(90, 25);
+            this.slDap.TabIndex = 4;
+            this.slDap.Text = "Secure";
+            this.slDap.UseVisualStyleBackColor = true;
+            this.slDap.CheckedChanged += new System.EventHandler(this.slDap_CheckedChanged);
+            // 
+            // DomainTxt
+            // 
+            this.DomainTxt.Location = new System.Drawing.Point(144, 76);
+            this.DomainTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.DomainTxt.Name = "DomainTxt";
+            this.DomainTxt.Size = new System.Drawing.Size(365, 22);
+            this.DomainTxt.TabIndex = 1;
+            this.DomainTxt.Text = "dc01.bites.local";
+            // 
+            // UsernameTxt
+            // 
+            this.UsernameTxt.Location = new System.Drawing.Point(144, 130);
+            this.UsernameTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.UsernameTxt.Name = "UsernameTxt";
+            this.UsernameTxt.Size = new System.Drawing.Size(365, 22);
+            this.UsernameTxt.TabIndex = 2;
+            this.UsernameTxt.Text = "elif.tozkoparan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 187);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // PasswordTxt
+            // 
+            this.PasswordTxt.Location = new System.Drawing.Point(144, 183);
+            this.PasswordTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.PasswordTxt.Name = "PasswordTxt";
+            this.PasswordTxt.PasswordChar = '*';
+            this.PasswordTxt.Size = new System.Drawing.Size(365, 22);
+            this.PasswordTxt.TabIndex = 3;
+            this.PasswordTxt.Text = "Ket";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 134);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username";
+            // 
+            // password_ADFS
+            // 
+            this.password_ADFS.Location = new System.Drawing.Point(120, 212);
+            this.password_ADFS.Margin = new System.Windows.Forms.Padding(4);
+            this.password_ADFS.Name = "password_ADFS";
+            this.password_ADFS.Size = new System.Drawing.Size(373, 22);
+            this.password_ADFS.TabIndex = 9;
+            // 
+            // username_ADFS
+            // 
+            this.username_ADFS.Location = new System.Drawing.Point(120, 153);
+            this.username_ADFS.Margin = new System.Windows.Forms.Padding(4);
+            this.username_ADFS.Name = "username_ADFS";
+            this.username_ADFS.Size = new System.Drawing.Size(373, 22);
+            this.username_ADFS.TabIndex = 8;
+            // 
+            // endPontTxt
+            // 
+            this.endPontTxt.Location = new System.Drawing.Point(120, 95);
+            this.endPontTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.endPontTxt.Name = "endPontTxt";
+            this.endPontTxt.Size = new System.Drawing.Size(885, 22);
+            this.endPontTxt.TabIndex = 7;
+            // 
+            // relyTxt
+            // 
+            this.relyTxt.Location = new System.Drawing.Point(120, 30);
+            this.relyTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.relyTxt.Name = "relyTxt";
+            this.relyTxt.Size = new System.Drawing.Size(885, 22);
+            this.relyTxt.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(139, 293);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(235, 62);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(41, 220);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 17);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Password";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(39, 95);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 17);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Endpoint";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 153);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 33);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Rely";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 444);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "LDAP Authentication Test";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TextBox password_ADFS;
+        private System.Windows.Forms.TextBox username_ADFS;
+        private System.Windows.Forms.TextBox endPontTxt;
+        private System.Windows.Forms.TextBox relyTxt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox portNumberTxt;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox slDap;
+        private System.Windows.Forms.TextBox DomainTxt;
+        private System.Windows.Forms.TextBox UsernameTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PasswordTxt;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices.Protocols;
+using System.IdentityModel.Protocols.WSTrust;
+using System.IdentityModel.Tokens;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Security;
+using System.Windows.Forms;
+
+namespace TestAD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (slDap.Checked)
+                {
+                    var connection = new LdapConnection(new LdapDirectoryIdentifier(DomainTxt.Text, Convert.ToInt32(portNumberTxt.Text)));
+                    connection.SessionOptions.SecureSocketLayer = true;
+                    connection.SessionOptions.ProtocolVersion = 3;
+                    connection.AuthType = AuthType.Negotiate;
+                    connection.SessionOptions.VerifyServerCertificate = new VerifyServerCertificateCallback((con, cer) => true);
+                    connection.Bind(new NetworkCredential(UsernameTxt.Text, PasswordTxt.Text, DomainTxt.Text));
+                }
+                else
+                {
+                    DirectoryEntry entry = new DirectoryEntry("LDAP://" + DomainTxt.Text, UsernameTxt.Text, PasswordTxt.Text);
+                    object nativeObject = entry.NativeObject;
+                }
+                MessageBox.Show("Success", "Authenticated");
+            }
+            catch (Exception ex)
+            {
+                if (slDap.Checked && ex.Message.Equals("The supplied credential is invalid."))
+                    MessageBox.Show(ex.Message + " ( Password or username not correct ) ", "Faild");
+                else
+                    MessageBox.Show(ex.Message, "Exception");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var binding = new WS2007HttpBinding(SecurityMode.TransportWithMessageCredential);
+
+            // If you set this to true, you can use a Security Context Token (SCT) for your next
+            // calls to the STS instead of sending a Username and Password each time.
+            binding.Security.Message.EstablishSecurityContext = false;
+
+            // Instead of using Username/Password authentication you can use any of the 
+            // other ClientCredentialType's such as Certificate, IssuedToken or Windows.
+            binding.Security.Message.ClientCredentialType = MessageCredentialType.UserName;
+
+            // The same applies to the transport level security. You can secure it in any 
+            // of the available ways.
+            binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.None;
+
+            var endpoint = new EndpointAddress(endPontTxt.Text);
+            var factory = new WSTrustChannelFactory(binding, endpoint);
+            factory.TrustVersion = TrustVersion.WSTrust13;
+            factory.Credentials.SupportInteractive = false;
+
+            // If your MessageCredentialType differs from UserName, make sure you supply 
+            // the valid credential here.
+            factory.Credentials.UserName.UserName = username_ADFS.Text;
+            factory.Credentials.UserName.Password = password_ADFS.Text;
+
+            var rst = new RequestSecurityToken()
+            {
+                RequestType = RequestTypes.Issue,
+                KeyType = KeyTypes.Bearer,
+                AppliesTo = new EndpointReference(relyTxt.Text),
+                TokenType = "urn:oasis:names:tc:SAML:2.0:assertion"
+            };
+
+            var channel = (WSTrustChannel)factory.CreateChannel();
+            RequestSecurityTokenResponse rstr;
+            try
+            {
+                SecurityToken token = channel.Issue(rst, out rstr);
+                MessageBox.Show("Success", "Authenticated");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception");
+            }
+        }
+
+        private void slDap_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
